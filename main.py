673d5b32a7f965c58b9bf0e8aa604c4c42d03cd6
glowcattrax <glowcattrax@gmail.com>
2026-01-22 01:16:24 +0500

    First lesson

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+weight = float(input("Введите вес"))
+i = str(input(f"'K'g or 'L'bs?"))
+
+if(i == "K" or i == "k"):
+    print(f"Ваш вес в фунтах: {weight*2.205}.")
+elif(i == "L" or i == "l"):
+    print(f"Ваш вес в килограммах: {weight/2.205}.")
+else:
+    print("Введено неверное значение")
